8a2ee900eaf6fd468945a7fcf1099e3de07a94f9
IvanKustarev <ivan.kustarev@gmail.com>
2022-05-09 18:15:30 +0300

    [in progress] Adding custom mappers

--- src/main/kotlin/me/ivan/articleBuilder/config/ModelMapperConfig.kt
+++ src/main/kotlin/me/ivan/articleBuilder/config/ModelMapperConfig.kt
@@ -1,17 +1,46 @@
 package me.ivan.articleBuilder.config
 
+import me.ivan.articleBuilder.controller.dto.AuthorDto
+import me.ivan.articleBuilder.controller.dto.EntDto
+import me.ivan.articleBuilder.model.Author
+import me.ivan.articleBuilder.model.BaseEntity
+import org.modelmapper.ExpressionMap
 import org.modelmapper.ModelMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ModelMapperConfig {
+
+    var mapper: ModelMapper = ModelMapper()
+
     @Bean
     fun modelMapper(): ModelMapper {
-        val modelMapper = ModelMapper()
 
 //        adding mapping configs in further
+        configAuthorMapping()
 
-        return modelMapper
+        return mapper
+    }
+
+    fun configAuthorMapping() {
+        propertyMapper<Author, AuthorDto>(
+            toDto = {mapper ->
+                mapper
+                    .map(Author::archived, AuthorDto::archived.setter)
+            },
+            fromDto = {mapper ->
+                mapper
+                    .map(AuthorDto::archived, Author::archived.setter)
+            }
+        )
+    }
+
+    private inline fun <reified E : BaseEntity<*>, reified D : EntDto<*>> propertyMapper(
+        toDto: ExpressionMap<E, D>,
+        fromDto: ExpressionMap<D, E>,
+    ) {
+         mapper.typeMap(E::class.java, D::class.java).addMappings(toDto)
+         mapper.typeMap(D::class.java, E::class.java).addMappings(fromDto)
     }
 }
